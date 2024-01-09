00beadf24340e770cbee485a30b33d15851ee716
ajaen4 <a.jaenrev@gmail.com>
2024-01-09 16:57:06 +0100

    Refactor partner uploader and add sityex own events

--- containers/partner_uploader/f_partner_uploader/__main__.py
+++ containers/partner_uploader/f_partner_uploader/__main__.py
@@ -1,10 +1,18 @@
+from datetime import datetime
+
 from f_partner_uploader.logger import logger
-from f_partner_uploader.processes.fever import upload_fever_data
-from f_partner_uploader.processes.own_events import upload_own_events
+from f_partner_uploader.processes.events import upload_events
 
 
 def main():
     logger.info("Starting upload to firestore...")
-    upload_fever_data()
-    # upload_own_events()
+
+    now = datetime.now()
+    TODAY_DATE = now.strftime("%d-%m-%Y")
+    FEVER_DATA_DIR = f"silver/partners/fever/catalog/{TODAY_DATE}/"
+    upload_events(partner="fever", events_data_dir=FEVER_DATA_DIR)
+
+    SITYEX_DATA_DIR = "maps/sityex_events.csv"
+    upload_events(partner="sityex", events_data_dir=SITYEX_DATA_DIR)
+
     logger.info("Finished upload to firestore")
--- containers/partner_uploader/f_partner_uploader/processes/common.py
+++ containers/partner_uploader/f_partner_uploader/processes/common.py
@@ -4,16 +4,12 @@ import f_partner_uploader.config as cfg
 
 
 def get_file_path(s3_client: S3, directory_path: str):
-    all_cities_paths = s3_client.list_files(
+    all_paths = s3_client.list_files(
         cfg.DATA_BUCKET_NAME,
         directory_path,
     )
 
-    if len(all_cities_paths) == 0:
+    if len(all_paths) == 0:
         return None
 
-    return [
-        all_cities_path
-        for all_cities_path in all_cities_paths
-        if all_cities_path.endswith(".csv")
-    ][0]
+    return [path for path in all_paths if path.endswith(".csv")][0]
