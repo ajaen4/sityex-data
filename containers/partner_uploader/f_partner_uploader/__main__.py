d0718af33b249f399666695f024afe8cdda06309
ajaen4 <a.jaenrev@gmail.com>
2024-03-02 21:07:28 +0100

    Fix: delete comments in code

--- containers/partner_uploader/f_partner_uploader/__main__.py
+++ containers/partner_uploader/f_partner_uploader/__main__.py
@@ -11,21 +11,21 @@ def main():
     now = datetime.now()
     TODAY_DATE = now.strftime("%d-%m-%Y")
 
-    # logger.info("Starting Fever upload...")
+    logger.info("Starting Fever upload...")
 
-    # FEVER_DATA_DIR = f"silver/partners/fever/catalog/{TODAY_DATE}/"
-    # upload_events(partner="fever", events_data_dir=FEVER_DATA_DIR)
+    FEVER_DATA_DIR = f"silver/partners/fever/catalog/{TODAY_DATE}/"
+    upload_events(partner="fever", events_data_dir=FEVER_DATA_DIR)
 
-    # logger.info("Finished Fever upload")
+    logger.info("Finished Fever upload")
 
-    # logger.info("Starting SityEx events upload...")
+    logger.info("Starting SityEx events upload...")
 
-    # SITYEX_DATA_DIR = "maps/sityex_events.csv"
-    # upload_events(partner="sityex", events_data_dir=SITYEX_DATA_DIR)
+    SITYEX_DATA_DIR = "maps/sityex_events.csv"
+    upload_events(partner="sityex", events_data_dir=SITYEX_DATA_DIR)
 
-    # logger.info("Finished SityEx events upload")
+    logger.info("Finished SityEx events upload")
 
-    # logger.info("Starting HousingAnywhere events upload...")
+    logger.info("Starting HousingAnywhere events upload...")
 
     HOUSING_ANYWHERE_DIR = f"silver/partners/housing_anywhere/{TODAY_DATE}/"
     upload_housing(partner="housing_anywhere", housing_data_dir=HOUSING_ANYWHERE_DIR)
