d8db9ad99aa11a4484b7d718de62780d8aad4347
ajaen4 <a.jaenrev@gmail.com>
2024-04-17 16:47:24 +0200

    Bump version of all containers after mypy inclusion

--- container_imgs/data_uploader/setup.py
+++ container_imgs/data_uploader/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="data_uploader",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=requirements,
 )
